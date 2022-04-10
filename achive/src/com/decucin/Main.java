1c1d818e309fd27751145609d73e3c88d12c319f
decucin <1690406856@qq.com>
2022-04-10 17:55:51 +0800

    增加了插入排序，希尔排序，归并排序，快速排序以及堆排序

--- achive/src/com/decucin/Main.java
+++ achive/src/com/decucin/Main.java
@@ -2,12 +2,20 @@ package com.decucin;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+
         int[] nums = Utils.createArray(10, 100);
         System.out.println("排序前：");
         Utils.printArray(nums);
+//        SortAlgorithms.bubbleSort(nums);
 //        SortAlgorithms.betterBubbleSort(nums);
-        SortAlgorithms.selectSort(nums);
+//        SortAlgorithms.selectSort(nums);
+//        SortAlgorithms.insertSort(nums);
+//        SortAlgorithms.betterInsertSort(nums);
+//        SortAlgorithms.shellSort(nums);
+//        int[] res = SortAlgorithms.mergeSort(nums);
+//        SortAlgorithms.quickSort(nums, 0, nums.length - 1);
+        SortAlgorithms.heapSort(nums);
         System.out.println("\n排序后：");
         Utils.printArray(nums);
     }
--- achive/src/com/decucin/Utils.java
+++ achive/src/com/decucin/Utils.java
@@ -6,6 +6,20 @@ import java.util.Random;
 
 public class Utils {
 
+    // swap函数，交换数组中i 和 j的值
+    public static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // 另一种不需要第三个变量就能实现的swap函数
+    public static void otherSwap(int[] nums, int i, int k){
+        nums[i] = nums[i] + nums[k];
+        nums[k] = nums[i] - nums[k];
+        nums[i] = nums[i] - nums[k];
+    }
+
     // 创建大小为size，数据范围在0-range的数组
     public static int[] createArray(int size, int range){
         Random random = new Random();
